757ba8363697e3537d1a05f723fa884ee4758d23
oguzcicek <soaoflisa@hotmail.com>
2020-12-04 20:34:19 +0300

    ilk deneme

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  get '/uk/blue-inc/blue-inc-in-bolton-unit-30-32-acresfield-mall-crompton-place-shopping-centre-opening-times', to: redirect('https://www.localworkingtimes.com/uk/catalogue/poundstretcher/poundstretcher-offers-01-30-dec-2020/offers', status: 301)
+  get '/post/poundstretcher-1', to: redirect('https://www.localworkingtimes.com/uk/catalogue/poundstretcher/poundstretcher-offers-01-30-dec-2020/offers', status: 301)
 
 
 
